4bd9ef60ef3c8ebc5825a3ebdedbb8e1649a378d
Aleksandr <ribakov.alex2013@yandex.ru>
2023-08-22 12:25:06 +0300

    add classmethod to get api and method convert class to json file

--- src/channel.py
+++ src/channel.py
@@ -6,14 +6,46 @@ import json
 class Channel:
     """Класс для ютуб-канала"""
 
-    api_key: str = os.getenv('YT_API_KEY')
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        youtube = self.get_service()
+        self._channel_id = channel_id
+        self._channel_info = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self._title = self._channel_info['items'][0]['snippet']['title']
+        self._description = self._channel_info['items'][0]['snippet']['description']
+        self._url = f'https://www.youtube.com/channel/{self._channel_id}'
+        self._subscribers = self._channel_info['items'][0]['statistics']['subscriberCount']
+        self._video_count = self._channel_info['items'][0]['statistics']['videoCount']
+        self._view_count = self._channel_info['items'][0]['statistics']['viewCount']
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        youtube = build('youtube', 'v3', developerKey=Channel.api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(channel)
+    @classmethod
+    def get_service(cls):
+        api_key = os.getenv('YT_API_KEY')
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def to_json(self, filename):
+        with open(filename, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        with open(filename, 'w', encoding='utf-8') as feedsjson:
+            entry = {
+                'title': self._title,
+                'description': self._description,
+                'url': self._url,
+                'subscribers': self._subscribers,
+                'video_count': self._view_count,
+                'view_count': self._view_count
+            }
+            data.append(entry)
+            json.dump(data, feedsjson, ensure_ascii=False)
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def video_count(self):
+        return self._video_count
